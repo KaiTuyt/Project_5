f4c733c710e87c35c4fbeb4d08e25e31726cbe64
KaiTuyt <jay.w.shoumaker-1@ou.edu>
2019-12-03 21:03:57 -0600

    Added GridPanes to my GridPane

--- Driver.java
+++ Driver.java
@@ -13,30 +13,32 @@ public class Driver extends Application {
 
 	@Override
 	public void start(Stage applicationStage) {
-		//Pane leftPane = new Pane();
-		TextField field = new TextField();
-		field.setEditable(false);
-		//leftPane.getChildren().add(field);
 		GridPane gridPane = new GridPane();
 		Insets gridPadding = new Insets(10, 10, 10, 10);
 		Scene display = new Scene(gridPane);
+		Label freeZone = new Label("FREE ZONE: You are free to fill this area with a creative idea.");
 		
+		GridPane topLeftPane = new GridPane();
 		Label enterLabel = new Label("Enter Hamming Distance: ");
 		TextField chosenDistance = new TextField();
 		chosenDistance.setEditable(false);
+		topLeftPane.add(enterLabel, 0, 0);
+		topLeftPane.add(chosenDistance, 1, 0);
+		
 		Slider slider = new Slider(1, 4, 2);
 		slider.setSnapToTicks(true);
 		slider.setShowTickMarks(true);
 		slider.setShowTickLabels(true);
 		slider.setMinorTickCount(0);
 		slider.setMajorTickUnit(1);
-		chosenDistance.setText(Integer.toString((int)slider.getValue()));
+		String chosen = Integer.toString((int)slider.getValue());
+		chosenDistance.setText(chosen);
 		Button showStation = new Button("Show Station");
 		
-		gridPane.add(enterLabel, 0, 0);
-		gridPane.add(chosenDistance, 1, 0);
+		gridPane.add(topLeftPane, 0, 0);
 		gridPane.add(slider, 0, 1);
 		gridPane.add(showStation, 0, 2);
+		gridPane.add(freeZone, 1, 0);
 		
 		gridPane.setPadding(gridPadding);
 		gridPane.setHgap(10);
@@ -45,7 +47,7 @@ public class Driver extends Application {
 		applicationStage.setScene(display);
 		applicationStage.setTitle("Hamming Distance");
 		applicationStage.show();
-		
+
 		
 	}
 
