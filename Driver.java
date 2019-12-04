e36cda218c94713fc20b148f327900b47d3a7e1f
KaiTuyt <jay.w.shoumaker-1@ou.edu>
2019-12-03 20:37:08 -0600

    Added very basic GUI components

--- Driver.java
+++ Driver.java
@@ -1,9 +1,27 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.TextField;
 
-public class Driver {
+public class Driver extends Application{
+	
+	@Override
+	public void start(Stage applicationStage) {
+		Pane leftPane = new Pane();
+		Scene display = new Scene(leftPane);
+		TextField field = new TextField();
+		field.setEditable(false);
+		leftPane.getChildren().add(field);
+		
+		applicationStage.setScene(display);
+		applicationStage.setTitle("Hamming Distance");
+		applicationStage.show();
+	}
 
 	public static void main(String[] args) {
 		
-		
+		launch(args);
 		
 	}
 
