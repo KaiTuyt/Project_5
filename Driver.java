bf6d1503606457e4a11e9b594a13fb312572e6fe
KaiTuyt <jay.w.shoumaker-1@ou.edu>
2019-12-03 22:01:15 -0600

    GUI mostly matches example

--- Driver.java
+++ Driver.java
@@ -2,12 +2,14 @@ import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 
@@ -16,15 +18,19 @@ public class Driver extends Application {
 	@Override
 	public void start(Stage applicationStage) {
 		GridPane gridPane = new GridPane();
-		Insets gridPadding = new Insets(10, 10, 10, 10);
+		Insets bigGridPadding = new Insets(10, 10, 10, 10);
+		Insets smallGridPadding = new Insets(5, 5, 5, 5);
 		Scene display = new Scene(gridPane);
 		
 		GridPane topLeftPane = new GridPane();
 		Label enterLabel = new Label("Enter Hamming Distance: ");
-		TextField chosenDistance = new TextField();
+		TextField chosenDistance = new TextField("2");
 		chosenDistance.setEditable(false);
 		topLeftPane.add(enterLabel, 0, 0);
 		topLeftPane.add(chosenDistance, 1, 0);
+		topLeftPane.setPadding(smallGridPadding);
+		topLeftPane.setHgap(10);
+		topLeftPane.setVgap(10);
 		
 		Slider slider = new Slider(1, 4, 2);
 		slider.setSnapToTicks(true);
@@ -40,21 +46,73 @@ public class Driver extends Application {
 			}
 		});
 				
-		Button showStation = new Button("Show Station");
+		Button showStationButton = new Button("Show Station");
+		//Add Mesonet.txt functionality...
 		
-		TextField availableStations = new TextField();
-		availableStations.setEditable(false);
+		TextField displayedStations = new TextField("PERK\n");
+		displayedStations.setEditable(false);
+		displayedStations.setPrefHeight(200);
+		displayedStations.setAlignment(Pos.TOP_LEFT);
+		//Add Mesonet.txt functionality...
 		
 		GridPane middleLeftPane = new GridPane();
+		Label compareWith = new Label("Compare with: ");
+		ComboBox stations = new ComboBox();
+		//Add Mesonet.txt functionality...
+		middleLeftPane.add(compareWith, 0, 0);
+		middleLeftPane.add(stations, 1, 0);
+		middleLeftPane.setPadding(smallGridPadding);
+		middleLeftPane.setHgap(10);
+		middleLeftPane.setVgap(10);
+		
+		Button calculateButton = new Button("Calculate HD");
+		//Add Mesonet.txt functionality...
+		
+		GridPane bottomLeftPane = new GridPane();
+		for (int i = 0; i < 5; i++) {
+			Label distanceLabel = new Label("Distance " + i);
+			bottomLeftPane.add(distanceLabel, 0, i);
+		}
+		TextField distance0 = new TextField("0");
+		distance0.setEditable(false);
+		bottomLeftPane.add(distance0, 1, 0);
+		TextField distance1 = new TextField("0");
+		distance1.setEditable(false);
+		bottomLeftPane.add(distance1, 1, 1);
+		TextField distance2 = new TextField("1");
+		distance2.setEditable(false);
+		bottomLeftPane.add(distance2, 1, 2);
+		TextField distance3 = new TextField("30");
+		distance3.setEditable(false);
+		bottomLeftPane.add(distance3, 1, 3);
+		TextField distance4 = new TextField("89");
+		distance4.setEditable(false);
+		bottomLeftPane.add(distance4, 1, 4);
+		//Add Mesonet.txt functionality...
+		Button addStationButton = new Button("Add Station");
+		//Add Mesonet.txt functionality...
+		bottomLeftPane.add(addStationButton, 0, 5);
+		TextField customStation = new TextField("ZERO");
+		customStation.setEditable(true);
+		//Add Mesonet.txt functionality...
+		bottomLeftPane.add(customStation, 1, 5);
+		bottomLeftPane.setPadding(bigGridPadding);
+		bottomLeftPane.setHgap(20);
+		bottomLeftPane.setVgap(20);
+
 		
 		Label freeZone = new Label("FREE ZONE: You are free to fill this area with a creative idea.");
 		
 		gridPane.add(topLeftPane, 0, 0);
 		gridPane.add(slider, 0, 1);
-		gridPane.add(showStation, 0, 2);
+		gridPane.add(showStationButton, 0, 2);
+		gridPane.add(displayedStations, 0, 3);
+		gridPane.add(middleLeftPane, 0, 4);
+		gridPane.add(calculateButton, 0, 5);
+		gridPane.add(bottomLeftPane, 0, 6);
 		gridPane.add(freeZone, 1, 0);
 		
-		gridPane.setPadding(gridPadding);
+		gridPane.setPadding(bigGridPadding);
 		gridPane.setHgap(10);
 		gridPane.setVgap(10);
 
